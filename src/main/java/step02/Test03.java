5c13212dfb9d20a600fb8010fc34ec211f8bdeb0
KJHak <bellcran@paran.com>
2017-08-20 12:52:59 +0900

    .

---
+++ src/main/java/step02/Test03.java
@@ -0,0 +1,10 @@
+/* 클래스와 소스 파일의 관계 II
+ * => 하나의 소스에 클래스 블록을 여러개 만들 수 있고,
+ * => 그 중 한개의 블록만이 public 으로 공개될 수 있다.
+ */
+package step02;
+
+public class Test03 {}
+class Test03_1 {}
+class Test03_2 {}
+
