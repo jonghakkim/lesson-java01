4866e060bfafab479c28d82b561eedb6ce9e7a6e
KJHak <bellcran@paran.com>
2017-08-20 13:23:23 +0900

    .

---
+++ src/main/java/step02/Test06.java
@@ -0,0 +1,19 @@
+/* 자바 프로그램을 실행시킬 때 시작점 (entry point)
+ * => 프로그램을 실행시킬 때 가장 처음 호출되고 실행되는 부분을 가리킨다.
+ * 
+ * 메서드 (=함수)
+ * => 함수 중에서 객체의 값을 다루고 연산하는 
+ *    명령어의 집합을 "method" 라고 한다.
+ * 
+ * main() 메서드
+ * => JVM(java, javac가 아님) 클래스 파일을 실행할 때 가장 먼저 실행하는 블록이다.
+ * => main() 메서드가 없으면 클래스를 jvm 이 실행할 수 없다.
+ */
+package step02;
+
+public class Test06 {
+  public static void main(String[] args) {
+    
+  }
+}
+
