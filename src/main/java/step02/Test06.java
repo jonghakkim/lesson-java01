9d203137cf7e5694e8aa70db7c68d151d1fb229b
KJHak <bellcran@paran.com>
2017-08-20 13:41:46 +0900

    .

--- src/main/java/step02/Test06.java
+++ src/main/java/step02/Test06.java
@@ -1,19 +1,22 @@
-/* 자바 프로그램을 실행시킬 때 시작점 (entry point)
- * => 프로그램을 실행시킬 때 가장 처음 호출되고 실행되는 부분을 가리킨다.
- * 
- * 메서드 (=함수)
- * => 함수 중에서 객체의 값을 다루고 연산하는 
- *    명령어의 집합을 "method" 라고 한다.
- * 
- * main() 메서드
- * => JVM(java, javac가 아님) 클래스 파일을 실행할 때 가장 먼저 실행하는 블록이다.
- * => main() 메서드가 없으면 클래스를 jvm 이 실행할 수 없다.
+/* 자바 프로그램을 표준 출력 장치로 출력하기
+ * => System.out.println() : OGNL표기법 (Object Graph Navigation Language)
+ * => System : 클래스
+ * => out 객체 : System 클래스 안에 있는 하나의 필드
+ * => print() : out 필드 안에 있는 메서드
+ *    => 그냥 출력만 한다.
+ * => println()
+ *    => 출력하고 줄바꿈한다. 
+ *    => "\n" String 필드에서 줄바꿈(enter)를 의미한다. 
  */
 package step02;
 
 public class Test06 {
   public static void main(String[] args) {
+    System.out.print("Hello,");
+    System.out.print("World!\n");
     
+    System.out.println("Hello,");
+    System.out.print("World!\n");
   }
 }
 
