f731d1421855e2efb3359d8f698296b42d978de0
KJHak <bellcran@paran.com>
2017-08-25 23:15:39 +0900

    .

--- src/main/java/step02/Test06.java
+++ src/main/java/step02/Test06.java
@@ -17,6 +17,23 @@ public class Test06 {
     
     System.out.println("Hello,");
     System.out.print("World!\n");
+    
+    
+    char c1 = 44032; // '가'의 유니코드 값 
+    System.out.println(c1);
+    // println() 에 char 타입의 값을 주면 유니코드로 인식한다.
+    // 그래서 폰트 파일에서 해당 코드의 문자 그림을 찾아 화면에 뿌린다.
+    System.out.println(44032);
+    
+    char c2 = 0xAC00; // '가'의 유니코드 값
+    System.out.println(c2);
+    
+    char c3 = '헐'; // '헐'의 유니코드 값을 리턴한다.
+      //작은 따옴표('')를 이용하면 문자의 유니코드 값을 알아 낼 수 있다.
+    System.out.println(c3);
+    
+    char c4 = '\uAC00'; // '가'의 유니코드 값을 리턴한다.
+    System.out.println(c4);
   }
 }
 
