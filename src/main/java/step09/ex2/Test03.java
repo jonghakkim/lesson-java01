487af28110649a9fea64b2d2195636102333aa82
jonghakkim <bellcran@naver.com>
2017-10-28 21:15:20 +0900

    .

---
+++ src/main/java/step09/ex2/Test03.java
@@ -0,0 +1,23 @@
+/* 상속 문법을 사용하기 전 III
+ * => 요구사항
+ *    - 우리는 AdvancedCalculator에 나누기 기능을 추가시키고 싶다.
+ * => 해결 방법
+ *    1) 새 클래스를 만들어 나누기 기능을 추가한다.
+ *    2) AdvancedCalculator의 기능을 사용하겠다고 선언한다. 
+ */ 
+package step09.ex2;
+
+public class Test03 {
+  public static void main(String[] args) {
+    MasterCalculator calc = new MasterCalculator();
+    //BasicCalculator calc = new MasterCalculator(); // 다형성
+    calc.plus(100);
+    calc.plus(20);
+    calc.minus(30);
+    calc.multiple(2);
+    //((MasterCalculator)calc).multiple(2); // 다형성
+    calc.divide(3);
+    //((MasterCalculator)calc).divide(3); // 다형성
+    System.out.println(calc.result);
+  }
+}
