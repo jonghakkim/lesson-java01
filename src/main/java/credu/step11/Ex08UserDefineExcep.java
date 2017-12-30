80ec09cff5baf1e8db337ff1c846b75b64c9e40c
jonghakkim <bellcran@naver.com>
2017-12-30 18:54:38 +0900

    .

--- src/main/java/credu/step11/Ex08UserDefineExcep.java
+++ src/main/java/credu/step11/Ex08UserDefineExcep.java
@@ -16,7 +16,7 @@ class UserNotAuthorizedException extends UsersException {
   private String name;
   // 생성자
   public UserNotAuthorizedException () {
-    super ( "이 사용자는 인증되지 않습니다.");
+    super ( "이 사용자는 존재하지 않습니다.");
   }
   public void setUid ( int i) {
     id = i;
@@ -37,7 +37,7 @@ class UidOutOfBoundException extends UsersException {
   private int hid;
   // 생성자 (인수는 UID 제한)
   public UidOutOfBoundException ( int i) {
-    super ( "UID의 범위는"+0 + "에서"+ i + "입니다.");
+    super ( "UID의 범위는 "+ 0 + " 에서 "+ i + "입니다.");
     hid = i;
   }
   public int getHid () {
@@ -48,7 +48,7 @@ class UidOutOfBoundException extends UsersException {
 class NullUsersException extends UsersException {
   // 생성자
   NullUsersException () {
-    super ( "사용자가 정의되어 있지 않습니다.");
+    super ( "사용자 목록이 존재하지 않습니다.");
   }
 }
 
@@ -104,7 +104,7 @@ public class Ex08UserDefineExcep {
       System. out .print ( "dominica ->");
       System. out .println (obj.getUser ( "dominica"));
       System. out .print ( "10 ->");
-      System. out .println (obj.getUser (10));
+      System. out .println (obj.getUser (2));
     } catch (UsersException e) {
       System. out .println ( "");
       System. out .println (e);
